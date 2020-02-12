7e30cff0447bd0275eda57edbfaae7d71cf1b21f
qasim683 <envoyfacilitation@gmail.com>
2020-02-12 16:10:29 +0500

    django

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from pages.views import home, contact, about
+
+urlpatterns = [
+    path('', home),
+    
+   
+    path('contact/', contact),
+    path('about/', about),
+]
