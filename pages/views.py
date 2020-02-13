f3fdc1625cbe91c7786cc819f02ecab7c7db0f29
qasim683 <envoyfacilitation@gmail.com>
2020-02-13 15:50:55 +0500

    db configration with mysql

--- pages/views.py
+++ pages/views.py
@@ -9,6 +9,12 @@ def about(request):
     return render(request, 'about.html',{'title': 'about Page Title'})
 
 def contact(request):
+    if(request.method == 'POST'):
+        email = request.POST ['email']
+        address = request.POST['address']
+        city = request.POST['city']
+        zipcode = request.POST['zip']
+        return render(request, 'contact.html', {'title': 'contact Page Title', 'email' : email, 'address' : address, 'city' : city, 'zipcode': zipcode } )
     return render(request, 'contact.html',{'title': 'contact Page Title'})
 
 def member(request, id):
