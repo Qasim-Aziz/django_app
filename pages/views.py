22f92ee33aa922d827b57b4adad7a358e36ecf21
qasim683 <49957560+qasim683@users.noreply.github.com>
2020-02-19 15:29:45 +0500

    using manual crud opreation

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from pages.models import Contact
 
 # Create your views here.
@@ -10,15 +10,34 @@ def about(request):
     return render(request, 'about.html',{'title': 'about Page Title'})
 
 def contact(request):
+    if(request.method=='GET' and request.GET.get('method')=='delete' and request.GET.get('id')):
+            rec = Contact.objects.filter(id=request.GET.get('id'))
+            rec.delete()
+    if(request.method=='GET' and request.GET.get('method')=='edit' and request.GET.get('id')):
+            rec = Contact.objects.filter(id=request.GET.get('id')).get()
+            return render(request, 'edit.html', {'title': 'edit Page Title','row' :rec })
+
     if(request.method == 'POST'):
-        data = Contact(
+        if(request.GET.get('method') == 'edit'):
+            rec = Contact.objects.filter(id=request.GET.get('id'))
+            rec.update(
+                name=request.POST['name'],
+                email=request.POST['email'],
+                address=request.POST['address'],
+                city=request.POST['city'],
+                zipcode=request.POST['zipcode']
+            )
+            return HttpResponseRedirect('/contact')
+        else:
+
+            data = Contact(
             name=request.POST['name'],
             email=request.POST['email'],
             address=request.POST['address'],
             city=request.POST['city'],
             zipcode=request.POST['zipcode']
-        )
-        data.save()
+            )
+            data.save()
     cnt= Contact.objects.all()
     return render(request, 'contact.html', {'title': 'contact Page Title','rows' :cnt } )
     
