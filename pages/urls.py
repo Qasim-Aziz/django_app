0501caf99ea6b01e4fe8653488418423e4ea1174
Qasim Aziz <49957560+Qasim-Aziz@users.noreply.github.com>
2024-05-31 16:03:35 +0500

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,7 @@ from pages.views import home, contact, about
 urlpatterns = [
     path('', home),
     
-   
+    path('mypage', mypage),   
     path('contact/', contact),
     path('about/', about),
 ]
